326df3ba4a2e1deda7428ea0570ba795feb67590
efilnikufesin <kristenstewartdada@gmail.com>
2020-05-26 01:26:58 +0000

    incoming call bar

---
+++ frontend/src/components/IncomingCalls.js
@@ -0,0 +1,50 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import Cookies from 'universal-cookie';
+
+class IncomingCalls extends Component {
+
+  static defaultProps = {
+  CallerID: '',
+  Channel: '',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      answered: false,
+      eventLoaded: false,
+    };
+  }
+
+    answer = e => {
+    this.setState({
+      answered: true
+      })
+    const cookies = new Cookies();
+    fetch("answerCall/", {
+      method: 'post',
+      headers: {"Content-Type":"application/json", "Authorization": `JWT ${localStorage.getItem('token')}`, "X-CSRFToken": cookies.get("csrftoken")},
+      body: JSON.stringify({
+        "channel": this.props.Channel,
+        })
+     });
+    console.log(this.props.Channel);
+  }
+
+
+  render() {
+
+  const {answer} = this;
+  
+    return (
+    <div className="event-handler">
+                 {this.props.CallerID} <button id="answer-btn" onClick={answer} value="Answer call"></button>
+    </div>
+    )
+
+  }
+}
+
+export default IncomingCalls;
